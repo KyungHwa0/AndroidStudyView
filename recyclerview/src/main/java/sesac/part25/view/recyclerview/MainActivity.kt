47fb30fb37eae623eabac6c64325ddf021579949
kyunghwa <snoopn303@gmail.com>
2023-03-22 20:45:03 +0900

    RecyclerView

--- recyclerview/src/main/java/sesac/part25/view/recyclerview/MainActivity.kt
+++ recyclerview/src/main/java/sesac/part25/view/recyclerview/MainActivity.kt
@@ -2,10 +2,22 @@ package sesac.part25.view.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import sesac.part25.view.recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val datas = mutableListOf<String>()
+        for (i in 1..10) {
+            datas.add("item $i")
+        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = MyAdapter(datas)
+        binding.recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
     }
 }
