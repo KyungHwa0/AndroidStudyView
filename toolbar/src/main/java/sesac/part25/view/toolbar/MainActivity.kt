aff1b18859f735f92364bd5d6b03513c79ae60da
kyunghwa <snoopn303@gmail.com>
2023-03-23 12:37:29 +0900

    Toolbar

--- toolbar/src/main/java/sesac/part25/view/toolbar/MainActivity.kt
+++ toolbar/src/main/java/sesac/part25/view/toolbar/MainActivity.kt
@@ -2,10 +2,20 @@ package sesac.part25.view.toolbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import sesac.part25.view.toolbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
