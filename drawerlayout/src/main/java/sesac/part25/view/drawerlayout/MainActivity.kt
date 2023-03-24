af2df7f1739cf9765c8e7ce9718d3ba825ac13b5
kyunghwa <snoopn303@gmail.com>
2023-03-24 22:30:12 +0900

    DrawerLayout

--- drawerlayout/src/main/java/sesac/part25/view/drawerlayout/MainActivity.kt
+++ drawerlayout/src/main/java/sesac/part25/view/drawerlayout/MainActivity.kt
@@ -2,10 +2,26 @@ package sesac.part25.view.drawerlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import sesac.part25.view.drawerlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.open, R.string.close)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
