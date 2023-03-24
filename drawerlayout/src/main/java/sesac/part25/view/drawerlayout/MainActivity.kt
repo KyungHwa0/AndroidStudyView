1195e83ec9a5458db4eb05840d270df49ee66754
kyunghwa <snoopn303@gmail.com>
2023-03-24 21:46:44 +0900

    DrawerLayout

---
+++ drawerlayout/src/main/java/sesac/part25/view/drawerlayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package sesac.part25.view.drawerlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
