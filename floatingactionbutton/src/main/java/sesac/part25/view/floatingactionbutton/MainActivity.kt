fccb313003ad82ce6b5a50c54c5850950d776c37
kyunghwa <snoopn303@gmail.com>
2023-03-24 21:43:34 +0900

    FloatingActionButton

--- floatingactionbutton/src/main/java/sesac/part25/view/floatingactionbutton/MainActivity.kt
+++ floatingactionbutton/src/main/java/sesac/part25/view/floatingactionbutton/MainActivity.kt
@@ -2,10 +2,19 @@ package sesac.part25.view.floatingactionbutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import sesac.part25.view.floatingactionbutton.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener {
+            when(binding.fab.isExtended){
+                true -> binding.fab.shrink()
+                false -> binding.fab.extend()
+            }
+        }
     }
 }
